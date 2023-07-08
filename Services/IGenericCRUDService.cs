9c83f9d512181cd0dba8b580c98d9506c3704b19
otamurod2003 <otamurodpirnapasov2003@gmail.com>
2023-07-08 20:07:14 +0500

    Added service to this project

---
+++ Services/IGenericCRUDService.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManagement.DataAccess;
+
+namespace LibraryManagement.Services
+{
+    public interface IGenericCRUDService<T> where T:class
+    {
+        Task<List<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Create(T librarian);
+        Task<T> Update(T librarian);
+        Task<bool> Delete(int id);
+    }
+}
---
+++ Services/LibrarianCRUDService.cs
@@ -0,0 +1,98 @@
+﻿using LibraryManagement.DataAccess;
+using LibraryManagement.Models;
+using System.Security.AccessControl;
+
+namespace LibraryManagement.Services
+{
+    public class LibrarianCRUDService : IGenericCRUDService<LibrarianModel>
+    {
+        private readonly ILibrarianRepository _librarianRepository;
+
+        public LibrarianCRUDService(ILibrarianRepository librarianRepository)
+        {
+            _librarianRepository = librarianRepository;
+        }
+        public async Task<LibrarianModel> Create(LibrarianModel model)
+        {
+            var librarian = new Librarian
+            {
+                FullName = model.FullName,
+                Age = model.Age,
+                LibraryDepartment = (DataAccess.Departments)model.LibraryDepartment,
+                PhotoFilePath = model.PhotoFilePath,
+            };
+            var createdLibrarian = await Task.FromResult(_librarianRepository.CreateLibrarian(librarian));
+            var result = new LibrarianModel
+            {
+                FullName = createdLibrarian.FullName,
+                Age = createdLibrarian.Age,
+                PhotoFilePath = createdLibrarian.PhotoFilePath,
+                LibraryDepartment = (Models.Departments)createdLibrarian.LibraryDepartment,
+            };
+            return result;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            return await _librarianRepository.DeleteLibrarian(id);                    
+        }
+
+        public async Task<LibrarianModel> Get(int id)
+        {
+            var librarian = await _librarianRepository.GetLibrarian(id);
+            var model = new LibrarianModel
+            {
+                //Id = librarian.Id,
+                FullName = librarian.FullName,
+                Age = librarian.Age,
+                LibraryDepartment = (Models.Departments)librarian.LibraryDepartment,
+                PhotoFilePath = librarian.PhotoFilePath,
+            };
+            return model;
+        }
+
+        public async Task<List<LibrarianModel>> GetAll()
+        {
+            var result = new List<LibrarianModel>();
+            var librarians = await _librarianRepository.GetAllLibrarians();
+            foreach(var librarian in librarians)
+            {
+                var model = new LibrarianModel  
+                {
+                    FullName = librarian.FullName,
+                    Age = librarian.Age,
+                    LibraryDepartment = (Models.Departments)librarian.LibraryDepartment,
+                    PhotoFilePath = librarian.PhotoFilePath,
+                    Id = librarian.Id
+                };
+
+                result.Add(model);
+            }
+            return result;
+        }
+
+        public async Task<LibrarianModel> Update(LibrarianModel model)
+        {
+            var librarian = new Librarian
+            {
+                FullName = model.FullName,
+                Age = model.Age,
+                PhotoFilePath = model.PhotoFilePath,
+                LibraryDepartment = (DataAccess.Departments)model.LibraryDepartment,
+            };
+            var updatedLibrarian =await  _librarianRepository.UpdateLibrarian(librarian);
+            var result = new LibrarianModel
+            {
+                FullName = updatedLibrarian.FullName,
+                Age = updatedLibrarian.Age,
+                LibraryDepartment = (Models.Departments)updatedLibrarian.LibraryDepartment,
+                PhotoFilePath = updatedLibrarian.PhotoFilePath,
+            };
+
+            return result;
+        }
+
+        
+    }
+}
+

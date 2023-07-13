56bbd73133010be4e8ca5c747a0752c691150784
otamurod2003 <otamurodpirnapasov2003@gmail.com>
2023-07-13 16:46:37 +0500

    implemented LibrarianCRUDService && built

--- LibraryManagement/Services/LibrarianCRUDService.cs
+++ LibraryManagement/Services/LibrarianCRUDService.cs
@@ -1,44 +1,44 @@
 ﻿using LibraryManagement.DataAccess;
 using LibraryManagement.DataAccess.Data;
 using LibraryManagement.Models;
-
-
 namespace LibraryManagement.Services
 {
     public class LibrarianCRUDService : IGenericCRUDService<LibrarianModel>
     {
-        private readonly IGenericRepository<LibrarianModel> _librarianRepository;
+        private readonly IGenericRepository<Librarian> _librarianRepository;
 
-        public LibrarianCRUDService(IGenericRepository<LibrarianModel> librarianRepository)
+        public LibrarianCRUDService(IGenericRepository<Librarian> librarianRepository)
         {
             _librarianRepository = librarianRepository;
         }
         public async Task<LibrarianModel> Create(LibrarianModel model)
         {
-            LibrarianModel librarian = new LibrarianModel
+            var librarian = new Librarian
             {
-                Id = model.Id,  
-                FullName = model.FullName,
+                Id = model.Id,
                 Age = model.Age,
-                LibraryDepartment = model.LibraryDepartment,
-                Adress
+                FullName = model.FullName,
+                LibraryDepartment = (DataAccess.Departments)model.LibraryDepartment,
+                AdressId = model.AdressId,
             };
-            var createdLibrarian =  await  Task.FromResult(_librarianRepository.Create(librarian));
-            var result = new Librarian
+            var createdLibrarian = await Task.FromResult(_librarianRepository.Create(librarian));
+            var result = new LibrarianModel
             {
+                Id = createdLibrarian.Id,
                 FullName = createdLibrarian.FullName,
                 Age = createdLibrarian.Age,
-                LibraryDepartment =  (DataAccess.Departments)createdLibrarian.LibraryDepartment,
-                Adress = 
-            }
-           
+                LibraryDepartment = (Models.Departments)createdLibrarian.LibraryDepartment,
+                AdressId = createdLibrarian.AdressId
+            };
+            return result;
 
-            
+
+
         }
 
         public async Task<bool> Delete(int id)
         {
-            return await _librarianRepository.Delete(id);                    
+            return await _librarianRepository.Delete(id);
         }
 
         public async Task<LibrarianModel> Get(int id)
@@ -46,11 +46,11 @@ namespace LibraryManagement.Services
             var librarian = await _librarianRepository.Get(id);
             var model = new LibrarianModel
             {
-               Id = librarian.Id,
+                Id = librarian.Id,
                 FullName = librarian.FullName,
                 Age = librarian.Age,
                 LibraryDepartment = (Models.Departments)librarian.LibraryDepartment,
-                //PhotoFilePath = librarian.PhotoFilePath,
+                AdressId = librarian.AdressId
             };
             return model;
         }
@@ -58,16 +58,17 @@ namespace LibraryManagement.Services
         public async Task<List<LibrarianModel>> GetAll()
         {
             var result = new List<LibrarianModel>();
-            var librarians = await _librarianRepository.GetAllLibrarians();
-            foreach(var librarian in librarians)
+            var librarians = await _librarianRepository.GetAll();
+            foreach (var librarian in librarians)
             {
-                var model = new LibrarianModel  
+                var model = new LibrarianModel
                 {
                     FullName = librarian.FullName,
                     Age = librarian.Age,
                     LibraryDepartment = (Models.Departments)librarian.LibraryDepartment,
                     //PhotoFilePath = librarian.PhotoFilePath,
-                    Id = librarian.Id
+                    Id = librarian.Id,
+                    AdressId = librarian.AdressId
                 };
 
                 result.Add(model);
@@ -79,26 +80,26 @@ namespace LibraryManagement.Services
         {
             var librarian = new Librarian
             {
-                //Id = model.Id,
+                Id = model.Id,
                 FullName = model.FullName,
                 Age = model.Age,
-                //PhotoFilePath = model.PhotoFilePath,
                 LibraryDepartment = (DataAccess.Departments)model.LibraryDepartment,
+                AdressId = model.AdressId,
             };
-            var updatedLibrarian =await  _librarianRepository.Update(librarian);
+            var updatedLibrarian = await _librarianRepository.Update(librarian);
             var result = new LibrarianModel
             {
-                //Id = updatedLibrarian.Id,
+                Id = updatedLibrarian.Id,
                 FullName = updatedLibrarian.FullName,
                 Age = updatedLibrarian.Age,
                 LibraryDepartment = (Models.Departments)updatedLibrarian.LibraryDepartment,
-                //PhotoFilePath = updatedLibrarian.PhotoFilePath,
+                AdressId = updatedLibrarian.AdressId
             };
 
             return result;
         }
 
-        
+
     }
 }
 
